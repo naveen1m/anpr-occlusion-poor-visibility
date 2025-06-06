4c9e135bd6bf1d1d64c6eb8cc1a533925e70cc6a
naveen <stupiddint@gmail.com>
2025-06-06 08:53:29 +0530

    building .py for anpr detection part, for complete pipeline

---
+++ np_detector/anpr.py
@@ -0,0 +1,47 @@
+
+# Commented out IPython magic to ensure Python compatibility.
+# %pip install ultralytics
+
+from ultralytics import YOLO
+import cv2
+import torch
+import requests
+import numpy as np
+
+# Constants
+SAVED_MODEL = "../models/my_yolo11n_model(new).pt"
+CONFIDENCE_THRESHOLD = 0.6
+
+# dataset_directory_path = "../data/IndianNumberPlate-dataset"
+
+def test_code():
+    print("test code here")
+# load saved model
+try:
+    trained_model = YOLO(SAVED_MODEL)
+except Exception as e:
+    print("Error loading model:", e)
+    exit(1)
+
+def detect_number_plate(image_path):
+    results = trained_model.predict(image_path,conf=0.6)  # Returns a list
+    for result in results:  # Loop through results (usually one item in the list)
+        result.show()  # Show the detected image
+        # Extract detailed predictions
+    detections = []
+    for box in results[0].boxes:
+        confidence = float(box.conf[0])
+        cls_id = int(box.cls[0])
+        xyxy = box.xyxy[0].tolist()
+        class_name = trained_model.names[cls_id]
+
+        detections.append({
+            "class": class_name,
+            "confidence": round(confidence, 3),
+            "box": [round(coord, 2) for coord in xyxy]
+        })
+
+    return detections
+
+
+
